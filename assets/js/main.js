24f681897fddfe557c104f3c3b06c505ff579277
Jean Silva <jeanpsilva10@gmail.com>
2022-08-26 19:45:28 -0300

    Atualizando mensagem e verificação

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,14 +14,21 @@ btnSubscribe.addEventListener('click', async function(){
     level: txtLevel.value,
     character: txtCharacter.value,
     }
+    if(txtName.value === '' ){
+        inscrito.innerHTML = `Você precisa digitar seu nome!`;
+        formTexto.style.display = 'none';
+        inscrito.style.display = 'flex';
+    } else{
     const subscriptionId = await subscribeToHellfireClub(subscription);
-    inscrito.innerHTML = `Agora você faz parte do Clube de D&D, ${txtName.value}!`;
+    inscrito.innerHTML = `Agora você faz parte do Clube de D&D! Seja bem-vindo ${txtName.value}!`;
     txtName.value = ''
     txtEmail.value = ''
     txtLevel.value = ''
     txtCharacter.value = ''
     formTexto.style.display = 'none';
     inscrito.style.display = 'flex';
+    }
+    
    
 })
 
